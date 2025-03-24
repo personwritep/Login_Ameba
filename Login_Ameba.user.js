8ff3732b62bb252a7f2d6316db820abc9843d784
Personwritep <89448434+personwritep@users.noreply.github.com>
2025-03-24 15:16:31 +0900

    Update Login_Ameba.user.js

--- Login_Ameba.user.js
+++ Login_Ameba.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Login Ameba
 // @namespace        http://tampermonkey.net/
-// @version        1.0
+// @version        1.1
 // @description        ブログページで自動ログイン
 // @author        Ameba Blog User
 // @match        https://ameblo.jp/*
@@ -53,21 +53,40 @@ if(location.hash!='#cbox'){ // #cbox付きURLで開いた場合は機能しな�
 
 
     function set_delay(){
+        let help_url='https://ameblo.jp/personwritep/entry-12891168337.html';
+
+        let SVG_la=
+            '<svg id="svg_la" viewBox="0 0 150 150">'+
+            '<path d="M66 13C56 15 47 18 39 24C-12 60 18 146 82 137C92 135 '+
+            '102 131 110 126C162 90 128 4 66 13M68 25C131 17 145 117 81 125C16 '+
+            '133 3 34 68 25M69 40C61 41 39 58 58 61C66 63 73 47 82 57C84 60 '+
+            '83 62 81 65C77 70 52 90 76 89C82 89 82 84 86 81C92 76 98 74 100 66'+
+            'C105 48 84 37 69 40M70 94C58 99 66 118 78 112C90 107 82 89 70 94z">'+
+            '</path></svg>';
+
         let topics=document.querySelector('._TiKJItsG');
         if(topics){ //「🛡️」ボタンを表示
             let sw=
                 '<div id="la"><div id="la_sw">🛡️</div>'+
                 '<div id="la_disp">Check Delay: '+
                 '<input id="la_set" type="number" min="0.5" max="10" step="0.5">'+
+                '<span id="la_sec">sec</span>'+
+                '<a href="'+ help_url +'" rel="noopener noreferrer" target="_blank">'+
+                SVG_la +'</a>'+
                 '</div>'+
                 '<style>'+
                 '#la { position: relative; z-index: 1; user-select: none; display: flex; } '+
                 '#la_sw { font-size: 26px; line-height: 40px; margin: 3px 10px 0 0; } '+
-                '#la_disp { font: normal 16px Meiryo; text-align: left; padding: 4px 12px 0; '+
-                'width: 180px; height: 35px; overflow: hidden; border: 1px solid #009688; '+
-                'color: #000; background: #eff5ff; box-shadow: 2px 2px 6px #00000050; } '+
-                '#la_set { font: normal 16px/23px Meiryo; margin-left: 6px; '+
-                'width: 50px; padding: 4px 2px 0 12px; border: revert; }'+
+                '#la_disp { position: relative; font: normal 16px Meiryo; text-align: left; '+
+                'padding: 4px 12px 0; width: 200px; height: 35px; overflow: hidden; '+
+                'color: #000; border: 1px solid #009688; background: #eff5ff; '+
+                'box-shadow: 2px 2px 6px #00000050; } '+
+                '#la_set { position: relative; font: normal 16px/23px Meiryo; caret-color: #fff; '+
+                'padding: 4px 7px 0 9px; width: 54px; border: revert; z-index: 1; } '+
+                '#la_set:hover { z-index: 3; } '+
+                '#la_sec { position: absolute; right: 38px; bottom: 6px; height: 24px; '+
+                'font-size: 16px; background: #fff; z-index: 2; } '+
+                '#svg_la { height: 16px; padding: 0 7px; fill: #028fff; vertical-align: -2px; } '+
                 '</style></div>';
 
             if(!topics.querySelector('#la')){
@@ -105,16 +124,16 @@ if(location.hash!='#cbox'){ // #cbox付きURLで開いた場合は機能しな�
 
         let la_set=la.querySelector('#la_set');
         if(la_set){
-            la_set.value=get_cookie('LA_delay')*(0.5);
+            la_set.value=(get_cookie('LA_delay')*(0.5)).toFixed(1);
 
-            la_set.onchange=(event)=>{
+            la_set.oninput=(event)=>{
                 event.preventDefault();
                 let set;
                 if(la_set.value<0.5){
                     set=1; }
                 else{
                     set=Math.round(la_set.value/0.5); }
-                la_set.value=set*(0.5);
+                la_set.value=(set*(0.5)).toFixed(1);
                 delay=set;
                 document.cookie='LA_delay='+ delay +'; path=/; Max-Age=604800'; }} // 7日
 
